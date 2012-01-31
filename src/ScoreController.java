4c9532132af7e4e43bea4da9b8dd0b662b8c6c1c
Jordan Ancar <Jordan Ancar@JordanAncar-PC>
2012-01-31 08:50:51 -0600

    testing changes

---
+++ src/ScoreController.java
@@ -0,0 +1,18 @@
+/*
+ * Yahtzee Score Controller class
+ * John Bohovic
+ * Started 1-25-12
+ * Last Modified 1-25-12
+ */
+
+
+public class ScoreController
+{
+   
+    public int firstRoll()
+    {
+        int[] firstRoll = Dice.rollAllDice();
+
+        return;
+    }
+}
