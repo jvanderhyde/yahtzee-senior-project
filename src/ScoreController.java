c80ea7b88259218603ce72f6de66025671f71c67
jamesvanderhyde <jamesvanderhyde@James-Vanderhydes-MacBook-Pro.local>
2012-02-08 14:24:55 -0600

    Disposed main window

---
+++ src/ScoreController.java
@@ -0,0 +1,29 @@
+/*
+ * Yahtzee Score Controller class
+ * John Bohovic
+ * Started 1-25-12
+ * Last Modified 1-25-12
+ */
+
+//Keep track of what boxes are still open, no entry can be made more than once
+public class ScoreController
+{
+    private int d1;
+    private int d2;
+    private int d3;
+    private int d4;
+    private int d5;
+
+    public void firstRoll()
+    {
+        int[] firstRoll = Dice.rollAllDice();
+
+        d1 = firstRoll[0];
+        d2 = firstRoll[1];
+        d3 = firstRoll[2];
+        d4 = firstRoll[3];
+        d5 = firstRoll[4];
+
+        
+    }
+}
