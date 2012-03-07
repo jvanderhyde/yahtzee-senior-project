ad933392af62709e9fa3f5c9813fc9b7301a56b1
john bohovic <john.bohovic@gmail.com>
2012-03-06 19:09:04 -0600

    Completed all of the category classes.

---
+++ src/welcomeMenu.java
@@ -0,0 +1,76 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.*;
+
+/**
+ *
+ * @author Jordan Ancar
+ */
+public class welcomeMenu
+{
+    JButton newGame;
+    JButton loadGame;
+    JButton quit;
+    JFrame frame;
+
+   public welcomeMenu()
+    {
+        frame = new JFrame("Welcome Menu");
+        frame.setSize(200, 200);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      
+        
+        JPanel jp = new JPanel();
+        jp.setLayout(new BorderLayout());
+        JLabel jl = new JLabel("Welcome Menu");
+        jl.setHorizontalAlignment(JLabel.CENTER);
+        jp.add(jl, BorderLayout.NORTH);
+
+        JPanel jp1 = new JPanel();
+        jp1.setLayout(new GridLayout(4,1));
+        newGame = new JButton("New Game");
+        newGame.addActionListener(new ActionListener()
+        {
+
+            public void actionPerformed(ActionEvent e) {
+                gameModeMenu g = new gameModeMenu();
+                frame.setVisible(false);
+
+                
+            }
+        });
+        loadGame = new JButton("Load Game");
+        quit = new JButton("Quit Game");
+        quit.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                frame.dispose();
+            }
+        });
+        jp1.add(newGame);
+        jp1.add(loadGame);
+        jp1.add(quit);
+        jp.add(jp1, BorderLayout.CENTER);
+
+        frame.add(jp);
+        frame.setVisible(true);
+    }
+
+
+
+}
